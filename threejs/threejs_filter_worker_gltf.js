d7c5374ad57fb36e1698f9e4b5a607d137a98176
migero <migero@gmail.com>
2020-07-24 10:23:22 +0200

    Update threejs_filter_worker_gltf.js

--- threejs/threejs_filter_worker_gltf.js
+++ threejs/threejs_filter_worker_gltf.js
@@ -50,6 +50,7 @@ function start( container, marker, video, input_width, input_height, canvas_draw
     renderer.setPixelRatio(window.devicePixelRatio);
 	renderer.gammaFactor = 2.2;
 	renderer.gammaOutput = true;
+	renderer.setSize( 1920, 1080 );
     var scene = new THREE.Scene();
 
     var camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
