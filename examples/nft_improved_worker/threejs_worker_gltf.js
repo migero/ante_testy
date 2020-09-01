fc8c886abca5ecd8192ce54441e9c6760c473482
migero <migero@gmail.com>
2020-09-01 14:10:55 +0200

    Update threejs_worker_gltf.js

--- examples/nft_improved_worker/threejs_worker_gltf.js
+++ examples/nft_improved_worker/threejs_worker_gltf.js
@@ -68,7 +68,7 @@ function start( container, marker, video, input_width, input_height, canvas_draw
 
     var scene = new THREE.Scene();
 
-    var camera = new THREE.Camera();
+    var camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 100);
     camera.matrixAutoUpdate = false;
 
     scene.add(camera);
